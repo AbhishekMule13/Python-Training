56d5ef1f1501315d68a35b1a3d7b8bece56cdc1b
Abhishek Mule <abhimule1308@gmail.com>
2025-07-17 11:05:25 +0530

    commit fibonissis

--- PythonTraining/Fibonacci sequence.py
+++ PythonTraining/Fibonacci sequence.py
@@ -1,11 +1,12 @@
-from operator import truediv
+#from operator import truediv
 
 
+''''
 def fibonacci_sequence(x):
     a=0
     b=1
     for s in range(x):
-        yield a
+       # yield a
         a , b = b ,a + b
 while True:
     try:
@@ -19,8 +20,31 @@ while True:
             print("Please enter positive number!!!")
 
     except ValueError:
-        print("Invalid input..")
+        print("Invalid input..")'''
 
+
+
+
+
+
+def fibonisis(x):
+    a=0
+    b=1
+    for i in range (x):
+        yield a
+        a,b=b,a+b
+while True:
+    try:
+        num=int(input(print("Enter the no of febonisis terms (Enter -1 for exit:)")))
+        if  num==-1:
+            print("exit")
+            break
+        elif num>0:
+            print(f"fibonaccies series is: {list(fibonisis(num))}")
+        else:
+            print("Please enter positive no!!!")    
+    except ValueError:
+           print("Invalid input")
 
 
 
