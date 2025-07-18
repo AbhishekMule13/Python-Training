2bba78ec6609ab0d51bcebae041c2bb7c78ba61c
Abhishek Mule <abhimule1308@gmail.com>
2025-07-18 19:16:42 +0530

    Game

---
+++ PythonTraining/GuessNo.py
@@ -0,0 +1,12 @@
+import random
+
+number=random.randint(1,10)
+
+while True:
+   Guess=input("Guess the number between (1 to 10):")
+   Guess=int(Guess)
+
+   if Guess == number:
+        print("You Win!!!")
+   else:
+        print("You Lose")    
