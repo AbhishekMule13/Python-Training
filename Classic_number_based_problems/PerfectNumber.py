1010796992bbcda0b87136ca93ca9d9f939373f7
Abhishek Mule <abhimule1308@gmail.com>
2025-07-29 08:36:54 +0530

    Create PerfectNumber.py

---
+++ Classic_number_based_problems/PerfectNumber.py
@@ -0,0 +1,15 @@
+while True:
+    num=int(input("Enter the number:"))
+
+    if num<1:
+        print("Please Enter the positive number")
+        continue
+    
+    total=0
+    for i in range(1, num):
+        if num%i==0:
+            total+=i
+    if total==num:
+        print(f"{num} is a perfect number")
+    else:
+        print(f"{num} is not a perfect number")        
