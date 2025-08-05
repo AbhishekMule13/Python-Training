2f2bb18c59dc2a9ba4ea5280e2b2db07eec79c07
Abhishek Mule <abhimule1308@gmail.com>
2025-08-05 22:19:09 +0530

    Harshad no

--- PythonTraining/AllOver.py
+++ PythonTraining/AllOver.py
@@ -152,6 +152,25 @@ while True:
         print(f"{num} is NOT a Spy number\n")
 '''
 
+#Harshad number
+'''
+while True:
+    num=int(input("Enter the number:"))
+    sum=0
+    temp=num
+    while temp>0:
+        digit=temp%10
+        sum+=digit
+        temp//=10
+
+    if num%sum==0:
+        print(f"{num} is Harshad number!!!")
+    elif sum==0:
+        print("Sum is divisiable")
+    else:
+        print(f"{num} is not Harshad number")
+'''
+
 
 
 
