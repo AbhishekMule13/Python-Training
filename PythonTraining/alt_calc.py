8539259bbd25e2823cd73ae5c268227090ed6ac1
Abhishek Mule <abhimule1308@gmail.com>
2025-08-09 23:15:05 +0530

    Create alt_calc.py

---
+++ PythonTraining/alt_calc.py
@@ -0,0 +1,28 @@
+
+#A 'n' number of list when in 1st no go to the stack then 2nd no add in 1st no then 3rd no substract to the addition of 1st and 2nd 
+#Example:::[12 13 14 18 17 12 11]=13
+while True:
+    num=list(map(int, input("Enter the list of num:(Sprated by spaces):").split()))
+    result=num[0]
+    for i in range(1,len(num)):
+        if i % 2!=0:
+            result+=i
+        else:
+            result-=i
+
+    print("Result is:", result)        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
