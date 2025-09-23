aa284150a9564bfc56fe21233536bb15afb9f1a3
Abhishek Mule <abhimule1308@gmail.com>
2025-09-23 15:10:49 +0530

    size check

--- PythonTraining/numpyVSlist.py
+++ PythonTraining/numpyVSlist.py
@@ -1,3 +1,4 @@
+'''
 import numpy as np
 import time
 
@@ -12,3 +13,13 @@ arr = np.arange(1_000_000)
 start = time.time()
 arr*2
 print("NumPy time:", time.time() - start)
+'''
+
+import sys, numpy as np
+
+lst = list(range(1000))
+arr = np.arange(1000)
+
+print("List size:", sys.getsizeof(lst))      # size of list container only (not elements)
+print("NumPy size:", arr.nbytes)             # actual memory used by all elements
+
